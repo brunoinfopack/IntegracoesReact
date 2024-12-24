08ae6004943550eca4cf86698510461eeb37b889
brunoinfopack <49312177+brunoinfopack@users.noreply.github.com>
2024-12-23 21:52:44 -0300

    Final

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,18 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@/constants': './constants',
+            '@/models': './models',
+            '@/styles': './styles',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+    ],
   };
 };
